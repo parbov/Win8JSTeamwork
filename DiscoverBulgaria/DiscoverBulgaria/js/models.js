30a2455c64dfd4a2b93f2016c054477f46214845
parbov <parbov@gmail.com>
2013-08-31 08:24:42 +0300

    added urls

--- DiscoverBulgaria/DiscoverBulgaria/js/models.js
+++ DiscoverBulgaria/DiscoverBulgaria/js/models.js
@@ -1,12 +1,14 @@
 ﻿(function () {
-    var PlaceModel = WinJS.Class.define(function (name, region, url) {
+    var PlaceModel = WinJS.Class.define(function (name, region, url, imgUrl) {
         this.name = name;
         this.region = region;
         this.url = url;
+        this.imgUrl = imgUrl;
     }, {
         name: "",
         region: "",
-        url: ""
+        url: "",
+        imgUrl:""
     })
 
     WinJS.Namespace.define("Models", {
--- DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
+++ DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
@@ -11,13 +11,13 @@
             // TODO: Initialize the page here.
 
 
-         //   WinJS.xhr({
-         //       url: "",
-         //       type: "GET"
-         //   }).then(function (request) {
-         //       var outputElement = document.getElementById("output");
-         //       outputElement.innerHTML = request.responseText
-         //   });
+        //    WinJS.xhr({
+        //        url: "http://100places.apphb.com/api/places/get-place?url=nana",
+        //        type: "GET"
+        //    }).then(function (request) {
+        //        var outputElement = document.getElementById("output");
+        //        outputElement.innerHTML = request.responseText
+        //    });
 
             WinJS.Binding.processAll(element,
                 ViewModels.places.getAt(options.indexInPlacesList));
