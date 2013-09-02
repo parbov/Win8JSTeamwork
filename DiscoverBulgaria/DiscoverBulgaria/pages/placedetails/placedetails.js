027252746f5a7b51c9e6cc8f24fa784f39354f30
parbov <parbov@gmail.com>
2013-09-02 10:37:43 +0300

    changes in data layer and consuming service

--- DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
+++ DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
@@ -10,13 +10,16 @@
         ready: function (element, options) {
             // TODO: Initialize the page here.
 
-            //    WinJS.xhr({
-            //        url: "http://100places.apphb.com/api/places/get-place?url=nana",
-            //        type: "GET"
-            //    }).then(function (request) {
-            //        var outputElement = document.getElementById("output");
-            //        outputElement.innerHTML = request.responseText
-            //    });
+            var x = ViewModels.places.getAt(options.indexInPlacesList);
+
+
+                WinJS.xhr({
+                    url: "http://100places.apphb.com/api/places/get-place?url="+x.url,
+                    type: "GET"
+                }).then(function (request) {
+                    var outputElement = document.getElementById("output");
+                    outputElement.innerHTML = request.responseText
+                });
 
             var savePlaylistBtn = document.getElementById("write-text");
             savePlaylistBtn.addEventListener("click", function () {
@@ -38,7 +41,6 @@
                 });
             });
 
-            var x = ViewModels.places.getAt(options.indexInPlacesList);
 
             WinJS.Binding.processAll(element, x);
 
