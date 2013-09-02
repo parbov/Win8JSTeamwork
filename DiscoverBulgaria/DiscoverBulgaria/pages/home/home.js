a362997f6a912240604bee6daf241b9e5d8e4f2e
Kaloyan Gospodinov <kaloyan_gospodinov@yahoo.co.uk>
2013-09-02 18:07:55 +0300

    CAMEEEEEEEEEEEEEEEEEEEERA

--- DiscoverBulgaria/DiscoverBulgaria/pages/home/home.js
+++ DiscoverBulgaria/DiscoverBulgaria/pages/home/home.js
@@ -9,6 +9,39 @@
         },
         ready: function (element, options) {
             // TODO: Initialize the page here.
+            this.updateLayout(element, Windows.UI.ViewManagement.ApplicationView.value, null);
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+
+            // Respond to changes in viewState.
+
+            // Get the ListView control. 
+        var viewStateExampleListView =
+            element.querySelector("#grid").winControl;
+
+            // Use a ListLayout if the app is snapped or in full-screen portrait mode. 
+        if (viewState === Windows.UI.ViewManagement.ApplicationViewState.snapped ||
+            viewState === Windows.UI.ViewManagement.ApplicationViewState.fullScreenPortrait) {
+
+            // If layout.Horizontal is false, the ListView
+            // is already using a ListLayout, so we don't
+            // have to do anything. We only need to switch
+            // layouts when layout.horizontal is true. 
+            if (viewStateExampleListView.layout.horizontal) {
+                viewStateExampleListView.layout = new WinJS.UI.ListLayout();
+            }
         }
+   
+            // Use a GridLayout if the app isn't snapped or in full-screen portrait mode. 
+        else 
+        {
+            // Only switch layouts if layout.horizontal is false. 
+            if (!viewStateExampleListView.layout.horizontal) {
+                viewStateExampleListView.layout = new WinJS.UI.GridLayout();
+            }
+        }
+
+    }
     });
 })();
--- DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
+++ DiscoverBulgaria/DiscoverBulgaria/pages/placedetails/placedetails.js
@@ -8,39 +8,19 @@
         // populates the page elements with the app's data.
         
         ready: function (element, options) {
-            // TODO: Initialize the page here.
 
             var x = ViewModels.places.getAt(options.indexInPlacesList);
 
-
-                WinJS.xhr({
-                    url: "http://100places.apphb.com/api/places/get-place?url="+x.url,
-                    type: "GET"
-                }).then(function (request) {
-                    var outputElement = document.getElementById("output");
-                    outputElement.innerHTML = request.responseText
-                });
-
-            var savePlaylistBtn = document.getElementById("write-text");
-            savePlaylistBtn.addEventListener("click", function () {
-                var ul = document.getElementById("placesText").innerHTML;
-                var savePicker = new Windows.Storage.Pickers.FileSavePicker();
-                savePicker.defaultFileExtension = ".txt"
-                savePicker.fileTypeChoices.insert("Plain Text", [".txt"])
-                savePicker.fileTypeChoices.insert("MS Word Text", [".doc"])
-                savePicker.suggestedFileName = document.getElementById("name").innerHTML;
-
-                savePicker.pickSaveFileAsync().then(function (file) {
-                    if (file) {
-                        Windows.Storage.FileIO.writeTextAsync(file, ul);
-                    }
-                    if (!file) {  // If the URL is really malformed or blank.
-                        var md = Windows.UI.Popups.MessageDialog("You haven't saved the file!");
-                        //md.showAsync()
-                    }
-                });
+            WinJS.xhr({
+                url: "http://100places.apphb.com/api/places/get-place?url="+x.url,
+                type: "GET"
+            }).then(function (request) {
+                var outputElement = document.getElementById("output");
+                outputElement.innerHTML = request.responseText
             });
 
+            document.getElementById("write-text").addEventListener("click", saveInfo,false);
+            document.getElementById("capturePhoto").addEventListener("click", capturePhoto, false);
 
             WinJS.Binding.processAll(element, x);
 
@@ -57,4 +37,40 @@
             // TODO: Respond to changes in viewState.
         }
     });
+
+    function saveInfo() {
+        var ul = document.getElementById("output").innerText;
+        var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+        savePicker.defaultFileExtension = ".txt"
+        savePicker.fileTypeChoices.insert("Plain Text", [".txt"])
+        savePicker.fileTypeChoices.insert("MS Word Text", [".doc"])
+        savePicker.suggestedFileName = document.getElementById("name").innerHTML;
+
+        savePicker.pickSaveFileAsync().then(function (file) {
+            if (file) {
+                Windows.Storage.FileIO.writeTextAsync(file, ul);
+            }
+        }, function (err) { 
+            var md = Windows.UI.Popups.MessageDialog("You haven't saved the file!");
+            //md.showAsync()
+        });
+    }
+
+    function capturePhoto() {
+        var captureUI = new Windows.Media.Capture.CameraCaptureUI();
+        captureUI.captureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.photo).then(function (capturedItem) {
+            if (capturedItem) {
+                var fileName = (document.getElementById("name").innerHTML) + ".jpg";
+                capturedItem.moveAsync(Windows.Storage.KnownFolders.picturesLibrary, fileName, Windows.Storage.NameCollisionOption.generateUniqueName)
+                var md = Windows.UI.Popups.MessageDialog("User captured a photo.");
+                md.showAsync()
+            }
+            else {
+                var md = Windows.UI.Popups.MessageDialog("User didn't capture a photo.");
+                md.showAsync()
+            }
+        });
+    }
+
+   
 })();
